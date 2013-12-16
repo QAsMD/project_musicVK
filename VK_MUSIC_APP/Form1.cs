5aba1f62559153d4d5518a4a27131fea1568525c
QAsMD <GinRomDam@gmail.com>
2013-12-16 13:14:06 +0400

    проверка

--- VK_MUSIC_APP/Form1.cs
+++ VK_MUSIC_APP/Form1.cs
@@ -106,7 +106,7 @@ namespace VK_MUSIC_APP
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            if (pl.IsPaused() == true) //privet
+            if (pl.IsPaused() == true) //
                 pl.Play(true);
             else
                 pl.Pause();
